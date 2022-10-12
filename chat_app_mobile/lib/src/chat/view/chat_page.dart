d5f0ee12e52f44a3da14cf3b5b2a7d99413d7833
quangnhat22 <wuangsotech@gmail.com>
2022-10-12 09:57:09 +0700

    update UI chat page

--- chat_app_mobile/lib/src/chat/view/chat_page.dart
+++ chat_app_mobile/lib/src/chat/view/chat_page.dart
@@ -1,6 +1,5 @@
+import 'package:auth_repository/auth_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -9,10 +8,104 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chat page'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 16,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(60),
+                child: Image.asset(
+                  'assets/images/Logo.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            const Expanded(
+              child: Text(
+                'CS Chat App',
+                overflow: TextOverflow.ellipsis,
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Chat page'),
+      body: Center(
+        child: FutureBuilder<List<User>>(
+          future: null,
+          builder: (context, snapshot) {
+            if (true) {
+              //return const CircularProgressIndicator();
+              return Padding(
+                padding: const EdgeInsets.all(64.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      "assets/images/ChatPlaceholder.jpeg",
+                      // width: 320,
+                      // height: 320,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          // border radius
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Find more friend',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return const Text('Chat app');
+            }
+          },
+        ),
       ),
     );
   }
