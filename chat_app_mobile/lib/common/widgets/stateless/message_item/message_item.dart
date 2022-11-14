bed81098a85d3886ca805b1ab459f7375e075219
quangnhat22 <wuangsotech@gmail.com>
2022-11-14 18:16:10 +0700

    upload image text

---
+++ chat_app_mobile/lib/common/widgets/stateless/message_item/message_item.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class IMessageItem extends StatelessWidget {
+  const IMessageItem({super.key});
+}
--- chat_app_mobile/lib/modules/chat_detail/widgets/message_item.dart
+++ chat_app_mobile/lib/modules/chat_detail/widgets/message_item.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app_mobile/common/widgets/stateless/message_item/factory_message_item.dart';
 import 'package:flutter/material.dart';
 
 // class MessageItem extends StatelessWidget {
@@ -88,25 +89,8 @@ class _MessageItemState extends State<MessageItem> {
               width: 8,
             )
           ],
-          Container(
-            constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.6),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-            decoration: BoxDecoration(
-              color: widget.isMe
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey[800]?.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Text(
-              widget.content,
-              style:
-                  TextStyle(color: widget.isMe ? Colors.white : Colors.black),
-            ),
-          ),
+          FactoryMessageItem.buildMessageItem(
+              "text", widget.isMe, widget.content),
         ],
       ),
     );
